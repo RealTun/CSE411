0353aae7185b0d71b4854935025ada13970c65cf
thanhst <thanhxk2003@gmail.com>
2024-11-30 17:53:38 +0700

    update api of Tuan God

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const userRouters = require('./src/routes/userRoutes');
+const pointRouters = require("./src/routes/api.routes");
 
 const port = config.port || 3000;
 
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(config.corsOptions));
 
-app.use("/api/user", userRouters)
+app.use("/api/user", userRouters);
+app.use("/api/point", pointRouters);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
---
+++ server/src/controller/api.controller.js
@@ -0,0 +1,127 @@
+const axios = require('axios');
+const https = require('https');
+
+const baseUrl = 'https://sinhvien1.tlu.edu.vn';
+
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
+// login
+const login = async (req, res, next) => {
+    try {
+        const apiUrl = `${baseUrl}/education/oauth/token`;
+
+        // Mẫu body request
+        // {
+        //     "client_id": "education_client",
+        //     "grant_type": "password",
+        //     "username": "",
+        //     "password": "",
+        //     "client_secret": "password"
+        // }
+
+        // Dữ liệu gửi đi từ body của client
+        const requestData = req.body;
+
+        // Gửi yêu cầu POST đến API
+        const response = await axios.post(apiUrl, requestData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            httpsAgent: agent,
+        });
+
+        // Trả kết quả từ API cho client
+        res.status(200).json({
+            message: 'Get token success',
+            token: `Bearer ${response.data['access_token']}`,
+            // data: response.data
+        });
+    } catch (error) {
+        res.status(error.response?.status || 500).json({
+            message: error.message,
+        });
+    }
+};
+
+const getCurrentStudent = async (req, res, next) => {
+    try {
+        const apiUrl = `${baseUrl}/education/api/users/getCurrentUser`;
+
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(400).json({
+                message: 'Authorization token is required',
+            });
+        }
+
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Authorization': token
+            },
+            httpsAgent: agent,
+        });
+
+        res.status(200).json({
+            message: 'Get current user success',
+            data: response.data,
+        });
+    } catch (error) {
+        res.status(error.response?.status || 500).json({
+            message: error.message
+        });
+    }
+};
+
+const getListMarkDetail = async (req, res, next) => {
+    try {
+        const apiUrl = `${baseUrl}/education/api/studentsubjectmark/getListMarkDetailStudent`;
+
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(400).json({
+                message: 'Authorization token is required',
+            });
+        }
+
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Authorization': token
+            },
+            httpsAgent: agent,
+        });
+
+        const idsToFind = [354, 358];
+        const filteredData = response.data.filter(item => 
+            item.subject && idsToFind.includes(item.subject.id)
+        );
+
+        const result = filteredData.map(item => {
+            const name = item.id === 354 ? 'Phân tích dữ liệu lớn' : 'Quản trị hệ thống thông tin'
+        
+            return {
+                subject: name,
+                mark: item.mark,
+                mark4: item.mark4,
+            };
+        });
+
+        res.status(200).json({
+            message: 'Get list mark success',
+            data: result
+        });
+    } catch (error) {
+        res.status(error.response?.status || 500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = {
+    login,
+    getCurrentStudent,
+    getListMarkDetail
+};
