94bff52e093a912e826753b8c3c8c2932c599b18
thanhst <thanhxk2003@gmail.com>
2024-12-01 19:56:12 +0700

    update code

--- main.py
+++ main.py
@@ -170,7 +170,6 @@ def predict(input_data):
 def get_chart_data():
     chart_data = data.to_dict(orient='records')
     return chart_data
-
 # Chạy ứng dụng
 eel.start('html/login.html',size=(1280,800))
 
