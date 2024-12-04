05b71ee004b22bbcecd1bf4f2a0bb02db0727349
thanhst <thanhxk2003@gmail.com>
2024-12-04 21:42:25 +0700

    update

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const pointRouters = require("./src/routes/api.routes");
 
 const port = config.port || 3000;
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(config.corsOptions));
